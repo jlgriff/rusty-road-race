c2f0c3701e45a01832586f9472aa70f3fc6ab302
John Griffith <john@John-MacBook-Air.local>
2022-07-05 00:26:22 -0400

    Added randomly appearing cars and obstacles.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use rusty_engine::prelude::*;
 
 struct GameState {
@@ -26,12 +27,71 @@ fn main() {
 
     // Create the road
     for i in 0..10 {
-        let roadline = game.add_sprite(
+        let road_line = game.add_sprite(
             format!("{ID_ROAD_LINE_SPRITE}{}", i),
             SpritePreset::RacingBarrierWhite,
         );
-        roadline.scale = 0.1;
-        roadline.translation.x = -600.0 + 150.0 * i as f32;
+        road_line.scale = 0.1;
+        road_line.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
+    // Create top barriers
+    for i in 0..10 {
+        let top_barrier = game.add_sprite(
+            format!("{ID_BARRIER_SPRITE}_top_{}", i),
+            SpritePreset::RacingBarrierWhite,
+        );
+        top_barrier.layer = 1.0;
+        top_barrier.scale = SCALE_BARRIER;
+        top_barrier.translation.x = WINDOW_MIN_X + 150.0 * i as f32;
+        top_barrier.translation.y = WINDOW_MAX_Y;
+        top_barrier.collision = true;
+    }
+
+    // Create bottom barriers
+    for i in 0..10 {
+        let bottom_barrier = game.add_sprite(
+            format!("{ID_BARRIER_SPRITE}_bottom_{}", i),
+            SpritePreset::RacingBarrierWhite,
+        );
+        bottom_barrier.layer = 1.0;
+        bottom_barrier.scale = SCALE_BARRIER;
+        bottom_barrier.translation.x = WINDOW_MIN_X + 150.0 * i as f32;
+        bottom_barrier.translation.y = WINDOW_MIN_Y;
+        bottom_barrier.collision = true;
+    }
+
+    // Create cars
+    let car_presets = vec![
+        SpritePreset::RacingCarBlack,
+        SpritePreset::RacingCarGreen,
+        SpritePreset::RacingCarRed,
+        SpritePreset::RacingCarYellow,
+    ];
+    for (i, preset) in car_presets.into_iter().enumerate() {
+        let car = game.add_sprite(format!("{ID_CAR_SPRITE}{}", i), preset);
+        car.layer = 5.0;
+        car.collision = true;
+        car.translation.x =
+            thread_rng().gen_range(WINDOW_MIN_OBSTACLE_GENERATION..WINDOW_MAX_OBSTACLE_GENERATION);
+        car.translation.y = thread_rng().gen_range((WINDOW_MIN_Y + 60.0)..(WINDOW_MAX_Y - 60.0));
+    }
+
+    // Create obstacles
+    let car_presets = vec![
+        SpritePreset::RollingHoleStart,
+        SpritePreset::RollingHoleEnd,
+        SpritePreset::RollingHoleStart,
+        SpritePreset::RollingHoleEnd,
+    ];
+    for (i, preset) in car_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("{ID_OBSTACLE_SPRITE}{}", i), preset);
+        obstacle.layer = 2.0;
+        obstacle.collision = true;
+        obstacle.translation.x =
+            thread_rng().gen_range(WINDOW_MIN_OBSTACLE_GENERATION..WINDOW_MAX_OBSTACLE_GENERATION);
+        obstacle.translation.y =
+            thread_rng().gen_range((WINDOW_MIN_Y + 60.0)..(WINDOW_MAX_Y - 60.0));
     }
 
     // Game logic functions are run each frame
@@ -71,10 +131,33 @@ fn player_movement_logic(engine: &mut Engine, game_state: &mut GameState) {
 
 fn road_movement_logic(engine: &mut Engine, game_state: &mut GameState) {
     for sprite in engine.sprites.values_mut() {
-        if sprite.label.starts_with(ID_ROAD_LINE_SPRITE) {
-            sprite.translation.x -= SPEED_ROAD_OBJECTS * engine.delta_f32;
+        // Road and barrier movement
+        if sprite.label.starts_with(ID_ROAD_LINE_SPRITE)
+            || sprite.label.starts_with(ID_BARRIER_SPRITE)
+        {
+            sprite.translation.x -= SPEED_ROAD * engine.delta_f32;
             if sprite.translation.x < WINDOW_MIN_X {
                 sprite.translation.x += (WINDOW_MAX_X * 2.0);
+            }
+        }
+        // Car movement
+        if sprite.label.starts_with(ID_CAR_SPRITE) {
+            sprite.translation.x -= SPEED_CARS * engine.delta_f32;
+            if sprite.translation.x < WINDOW_MIN_X - 200.0 {
+                sprite.translation.x = thread_rng()
+                    .gen_range(WINDOW_MIN_OBSTACLE_GENERATION..WINDOW_MAX_OBSTACLE_GENERATION);
+                sprite.translation.y =
+                    thread_rng().gen_range((WINDOW_MIN_Y + 60.0)..(WINDOW_MAX_Y - 60.0));
+            }
+        }
+        // Obstacle movement
+        if sprite.label.starts_with(ID_OBSTACLE_SPRITE) {
+            sprite.translation.x -= SPEED_ROAD * engine.delta_f32;
+            if sprite.translation.x < WINDOW_MIN_X - 200.0 {
+                sprite.translation.x = thread_rng()
+                    .gen_range(WINDOW_MIN_OBSTACLE_GENERATION..WINDOW_MAX_OBSTACLE_GENERATION);
+                sprite.translation.y =
+                    thread_rng().gen_range((WINDOW_MIN_Y + 60.0)..(WINDOW_MAX_Y - 60.0));
             }
         }
     }
@@ -83,12 +166,21 @@ fn road_movement_logic(engine: &mut Engine, game_state: &mut GameState) {
 // ID constants
 const ID_PLAYER_SPRITE: &str = "player";
 const ID_ROAD_LINE_SPRITE: &str = "road_line";
+const ID_BARRIER_SPRITE: &str = "barrier";
+const ID_OBSTACLE_SPRITE: &str = "obstacle";
+const ID_CAR_SPRITE: &str = "car";
 
-// Game constants
+// Graphical constants
 const WINDOW_MIN_Y: f32 = -360.0;
 const WINDOW_MAX_Y: f32 = 360.0;
 const WINDOW_MIN_X: f32 = -675.0;
 const WINDOW_MAX_X: f32 = 675.0;
+const SCALE_BARRIER: f32 = 0.7;
+const WINDOW_MIN_OBSTACLE_GENERATION: f32 = 700.0;
+const WINDOW_MAX_OBSTACLE_GENERATION: f32 = 1800.0;
+
+// Game constants
 const VOLUME_MUSIC: f32 = 0.05;
 const SPEED_PLAYER: f32 = 300.0;
-const SPEED_ROAD_OBJECTS: f32 = 400.0;
+const SPEED_ROAD: f32 = 900.0;
+const SPEED_CARS: f32 = 250.0;
